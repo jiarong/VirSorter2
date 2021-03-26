ec8ee3c5a5a79e71e1b666b3ac18cbf9f4939669
jiarong <guojiaro@gmail.com>
2021-03-25 22:03:55 -0400

    Change to use setup.cfg, setup.py and pyproject.toml instead of setup.py alone

--- setup.py
+++ setup.py
@@ -1,55 +1,5 @@
-import os
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+import setuptools
 
-
-# read the contents of your README file
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.md')) as f:
-    long_description = f.read()
-
-
-def get_version(relpath):
-    '''Read version info from a file without importing it'''
-    for line in open(os.path.join(os.path.dirname(__file__), relpath)):
-        if '__version__' in line:
-            v = line.strip().split('=', 1)[1]
-            v = v.strip().strip('"\' ')
-            return v
-        else:
-            sys.stderr.write('*** No version info found..\n')
-            return ''
-
-setup(
-    name='virsorter',
-    version=get_version("virsorter/__init__.py"),
-    url='https://github.com/jiarong/VirSorter2',
-    license='GPL-2',
-    author='Jiarong Guo',
-    author_email='guojiaro@gmail.com',
-    description=('VirSorter2: A multi-classifier, expert-guided approach to '
-        'detect diverse DNA and RNA virus genomes'),
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=['virsorter'],
-    # use MANIFEST.in instead
-    #package_data={
-    #    #'': ["virsorter/*", ]  
-    #    # include anything under virsorter; 
-    #    'virsorter': ['*']
-    #},
-    # include_package_data flag controls whether non-python files 
-    #  distributed in the sdist AND existing in a package 
-    #  (directory with __init__.py) install to the site-packages location. 
-    #  So in order for non-python files to install with your code 
-    #  they need to a) be in the sdist (controlled by MANIFEST.in) and 
-    #  b) exist inside an installable python package.
-    include_package_data=True,  # include all files in MANIFEST.in
-    data_files=[],
-    install_requires= [], # install via conda instead
-    entry_points={
-          'console_scripts': [
-              'virsorter = virsorter.virsorter:cli'
-          ]
-    },
-    classifiers=["Topic :: Scientific/Engineering :: Bioinformatics"],
-)
+if __name__ == '__main__':
+    setuptools.setup()
