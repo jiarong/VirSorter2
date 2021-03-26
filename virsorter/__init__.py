44f8117ec30d1a0d12af3096d06d99d3f525ed2d
jiarong <guojiaro@gmail.com>
2021-03-25 20:32:39 -0400

    Change to use importlib to parse version info from setup.cfg

--- virsorter/__init__.py
+++ virsorter/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '2.1'
+try:
+    from importlib import metadata
+except ImportError as e:
+    import importlib_metadata as metadata
+
+#__version__ = '2.1'
+__version__ = metadata.version('virsorter')
